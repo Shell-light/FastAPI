62289a64d580e273681d3bcf77796d7c11a0bfe1
Light <ulightshell@gmail.com>
2025-02-20 11:15:03 +0500

    Create Posts

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from sqlalchemy import TIMESTAMP, Column, Integer, String, Boolean, text
 
 
+
 from .database import Base
+
 
 class Post(Base):
     __tablename__ = "posts"
@@ -10,4 +12,5 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean,server_default='TRUE', nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    created_at = Column(TIMESTAMP(timezone=True), 
+                        nullable=False, server_default=text('now()'))
